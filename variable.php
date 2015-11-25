55aa6762987caba7d06f517c4af1e5d01a0bda70
akandshuvo <akandshuvo@gmail.com>
2015-11-25 10:16:21 +0600

    Boolean

---
+++ variable.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Variables</title>
+</head>
+<body>
+	
+	<?php 
+		//Boolean
+		$a= true; // returns 1
+		$b=false; // return 0
+		echo $a.$b
+	 ?>>
+
+</body>
+</html>
