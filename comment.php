577d2448d5b0328a759d6c277fe3dc652300b353
akandshuvo <akandshuvo@gmail.com>
2015-11-25 09:49:01 +0600

    comment-1

---
+++ comment.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>comment</title>
+</head>
+<body>
+	
+ 		<?php
+ 		// Printing Variable value
+            $print =5;
+            echo $print;
+        ?>
+
+        <br>
+
+        <%
+            echo "ASP tag";
+        %>
+
+        <br>
+
+        <?
+            $a=10;
+            $b=20;
+            $c=$a+$b;
+            echo $c;
+        ?>
+
+        <br>
+        
+        <script language="php">
+            $name="Shovon Rahman";
+            $id ="108012";
+
+            echo $name."&nbsp;".">>>".$id
+        </script>
+
+
+</body>
+</html>
