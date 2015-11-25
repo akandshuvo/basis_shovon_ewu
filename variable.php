b7befde3b7dab087de1fa9ea084a44a0a594cbd5
akandshuvo <akandshuvo@gmail.com>
2015-11-25 10:23:46 +0600

    float

--- variable.php
+++ variable.php
@@ -10,17 +10,22 @@
 		//Boolean
 		$a= true; // returns 1
 		$b=false; // return 0
-		echo $a.$b
-
+		echo $a;
+		echo "<br>";
 		//integer
-		$print =5;
+		$print = 5;
         echo $print;
-
+        echo "<br>";
 		//String
 		$name="Shovon Rahman";
         $id ="108012";
 
-        echo $name."&nbsp;".">>>".$id
+        echo $name."&nbsp;".">>>".$id;
+        echo "<br>";
+        //float
+		$float =5.123457489;
+        echo $float;
+
 
 	 ?>
 
