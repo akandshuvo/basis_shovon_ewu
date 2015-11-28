3354a6f264663597fd99c78147dde10fd4157697
akandshuvo <akandshuvo@gmail.com>
2015-11-28 13:00:02 +0600

    28-11-2015-finish

---
+++ 28-11-2015/theory.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>28-11-2015</title>
+</head>
+<body>
+	
+<ul>
+	<li>Add meaningful context---> page(28)</li>
+	<li>Single Responsibility Pronciple(SRP) </li>
+	<li>Open Closed Principle(OCP)</li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+
+</ul>
+
+
+</body>
+</html>
