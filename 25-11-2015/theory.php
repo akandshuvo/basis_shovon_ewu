1e1d9fdac963e7806639fe48c8e4a0e185141735
akandshuvo <akandshuvo@gmail.com>
2015-11-28 08:44:04 +0600

    28-11-2015_BASIS

--- 25-11-2015/theory.php
+++ 25-11-2015/theory.php
@@ -12,3 +12,26 @@ variable Type:
  -------------
  >> array
  >> object
+
+ php function:
+---------------------
+>>floatval
+>>empty
+>>is_array
+>>is_null
+>>is_object
+>>isset
+>>print_r
+>>serialize
+>>unserialize
+>>unset
+>>var_dump
+>>var_export
+>>gettype
+>>is_bool
+>>is_float
+>>is_string
+>>is_int
+>>boolval
+>>intval
+>>is_scalar
