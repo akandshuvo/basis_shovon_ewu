50a6bd1d3689c6cc6fd1e560b620fe4e13c22745
akandshuvo <akandshuvo@gmail.com>
2015-11-30 10:13:59 +0600

    BASIS-30NOV-2nd

--- 30-11-2015/trait.php
+++ 30-11-2015/trait.php
@@ -2,11 +2,31 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>traits</title>
+	<title></title>
 </head>
 <body>
 	
+<?
+//TRAITS
+	trait trait_use{
+		public function welcome(){
+			echo "Hello, oilcome to BASIS";
+		}
 
+	}
+
+/**
+	* 
+	*/
+	class greetings 
+	{
+		
+		use trait_use;
+	}	
+
+	greetings::welcome();
+
+?>
 
 </body>
 </html>
